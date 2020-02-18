42196e0685997644eb474904176a907efdb8a16b
Gary Russell <rugary@amazon.com>
2020-02-18 11:29:33 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.  This has changed. MASTER
+          Edit <code>src/App.js</code> and save to reload.  This has changed. MASTERsdfgsdfg 
         </p>
         <a
           className="App-link"
