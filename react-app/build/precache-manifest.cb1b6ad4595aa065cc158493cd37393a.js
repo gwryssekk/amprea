9827f451af34919de07f0cc4681e6d0f98356374
Gary Russell <rugary@amazon.com>
2020-04-15 21:10:58 -0700

    1587010258: removing node modules

---
+++ react-app/build/precache-manifest.cb1b6ad4595aa065cc158493cd37393a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e0cdcf6f8ece95c2ccca5615454e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "107fa1bd6ff4663b3736",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c146ca3d6477ddb2b3ba",
+    "url": "/static/js/2.d9d1fcfa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d9d1fcfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "107fa1bd6ff4663b3736",
+    "url": "/static/js/main.2e714016.chunk.js"
+  },
+  {
+    "revision": "7337c61acc0817764fea",
+    "url": "/static/js/runtime-main.17253fd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ react-app/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.  This has changed. MASTERdos
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+{/*1582316779*/}
+{/*1582316783*/}
+{/*1582317108*/}
+{/*1582317112*/}
+{/*1582317434*/}
+{/*1582317437*/}
+{/*1582317681*/}
+{/*1582317684*/}
+{/*1582317690*/}
+{/*1582319574*/}
+{/*1582319577*/}
+{/*1582319580*/}
+{/*1582319714*/}
+{/*1582319717*/}
+{/*1582319720*/}
