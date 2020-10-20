cfa10e4ad0bd679c8f8826a3e490269f775c0cc2
gwryssekk <rugary@amazon.com>
2020-10-20 15:24:00 -0700

    pr

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.  This has changed. MASTERdos
+          Edit <code>src/App.js</code> and save to reload.  This has changed. Pull request
         </p>
         <a
           className="App-link"
