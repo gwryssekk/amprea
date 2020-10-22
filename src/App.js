00a06eab2f3bfd6aa2229bdbaafd9a09a7602cb7
gwryssekk <rugary@amazon.com>
2020-10-22 15:22:22 -0700

    redep

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.  This has changed. Pull request.  Please
+          Edit <code>src/App.js</code> and save to reload.  This has changed. Pull request.  Please.  Redeploy.
         </p>
         <a
           className="App-link"
